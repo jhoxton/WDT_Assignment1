ebfb242498f2c3d798154422381571c33f60bfc6
jhoxton <jakehnovak@gmail.com>
2018-03-17 14:02:35 +1100

    Intial check-in

---
+++ MainClass.cs
@@ -0,0 +1,261 @@
+﻿using System;
+namespace Assignment1
+{
+    public class MainClass
+    {
+
+        SET UP A GIT, YOU GIT
+        static void Main(string[] args)
+        {
+            mainMenu();           
+        }
+        static void mainMenu()  //The main menu method
+        {
+            try
+            {
+                int choice = 0;
+                while (true)
+                {
+                    Console.WriteLine("Welcome to Marvelous Magic\n==============");
+                    Console.WriteLine("1. Owner");
+                    Console.WriteLine("2. Franchise Holder");
+                    Console.WriteLine("3. Customer");
+                    Console.WriteLine("4. Quit");
+
+                    string userinput = Console.ReadLine();
+
+                    //User input validation
+                    int userChoice = 0;
+                    if (int.TryParse(userinput, out userChoice))//Turns user inout from a string to an int
+                    {
+                        //if (userChoice < 1 || userChoice > 4)
+                        //{
+                        //    Console.WriteLine("Invalid number entered , please enter between 1 and  4 ...");
+                        //    //choice = 0;
+                        //}
+                        choice = userChoice;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please input number only between 1 and 4");
+                        choice = 0;
+                    }
+                    //This is the old menu logic
+                    //if (choice == 1)
+                    //    ownerMenu();
+                    //if (choice == 2)
+                    //    franchiseMenu();
+                    //if (choice == 3)
+                    //    customerMenu();
+                    //if (choice == 4)
+                        //Environment.Exit(0);
+                    
+                    switch (choice)
+                    {
+                        case 1:
+                            choice = 1;
+                            ownerMenu();
+                            break;
+                        case 2:
+                            choice = 2;
+                            franchiseMenu();
+                            break;
+                        case 3:
+                            choice = 3;
+                            customerMenu();
+                            break;
+                        case 4:
+                            Environment.Exit(0);
+                            break;
+                        default:
+                            break;
+                    } //End Switch
+
+                } // end of while
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("System Exception : " + e.Message);
+            }
+
+        } // end of mainMenu
+
+        static void ownerMenu()
+        {
+            try
+            {
+
+                int choice = 0;
+                while (true)
+                {
+                    Console.WriteLine("Welcome to Marvelous Magic(Owner)\n==============");
+                    Console.WriteLine("1. Display All Stock Requests");
+                    Console.WriteLine("2. Display Owner Inventory ");
+                    Console.WriteLine("3. Reset Inventory Item Stock");
+                    Console.WriteLine("4. Return to main menu");
+
+                    string userinput = Console.ReadLine();
+
+                    //validation here
+                    int userChoice = 0;
+                    if (int.TryParse(userinput, out userChoice))
+                    {
+                        choice = userChoice;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please input number only between 1 and 4");
+                        choice = 0;
+                    }
+                    switch (choice)
+                    {
+                        case 1:
+                            choice = 1;
+                            //TODO
+                            break;
+                        case 2:
+                            choice = 2;
+                            //TODO
+                            break;
+                        case 3:
+                            choice = 3;
+                            //TODO
+                            break;
+                        case 4:
+                            mainMenu();
+                            break;
+                        default:
+                            break;
+                    } //End Switch
+                } // end of while
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("System Exception : " + e.Message);
+            }
+
+
+
+        } // end of ownerMenu
+
+        static void franchiseMenu()
+        //Prompt for Store ID here first
+        {
+            try
+            {
+                int choice = 0;
+                while (true)
+                {
+                    Console.WriteLine("Welcome to Marvelous Magic(Franchise Holder)\n==============");
+                    Console.WriteLine("1. Display Inventory");
+                    Console.WriteLine("2. Stock Request(Threshold)");
+                    Console.WriteLine("3. Add New Inventory Item");
+                    Console.WriteLine("4. Return to main menu");
+
+                    string userinput = Console.ReadLine();
+
+                    //validation here
+                    int userChoice = 0;
+                    if (int.TryParse(userinput, out userChoice))
+                    {
+                        choice = userChoice;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please input number only between 1 and 4");
+                        choice = 0;
+                    }
+
+                    switch (choice)
+                    {
+                        case 1:
+                            choice = 1;
+                            //TODO
+                            break;
+                        case 2:
+                            choice = 2;
+                            //TODO
+                            break;
+                        case 3:
+                            choice = 3;
+                            //TODO
+                            break;
+                        case 4:
+                            mainMenu();
+                            break;
+                        default:
+                            break;
+                    } //End Switch
+                } // end of while
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("System Exception : " + e.Message);
+            }
+        }//end of franchiseMenu
+
+        static void customerMenu()
+        {
+            //Prompt for store ID here
+            try
+            {
+                int choice = 0;
+                while (true)
+                {
+                    Console.WriteLine("Welcome to Marvelous Magic(Retail)\n==============");
+                    Console.WriteLine("1. Display Products");
+                    Console.WriteLine("4. Return to main menu");
+
+                    string userinput = Console.ReadLine();
+
+                    //validation here
+                    int userChoice = 0;
+                    if (int.TryParse(userinput, out userChoice))
+                    {
+                        choice = userChoice;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please input number only between 1 and 4");
+                        choice = 0;
+                    }
+
+                    switch (choice)
+                    {
+                        case 1:
+                            choice = 1;
+                            //TODO
+                            break;
+                        case 2:
+                            choice = 2;
+                            //TODO
+                            break;
+                        case 3:
+                            choice = 3;
+                            //TODO
+                            break;
+                        case 4:
+                            mainMenu();
+                            break;
+                        default:
+                            break;
+                    } //End Switch
+                } // end of while
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("System Exception : " + e.Message);
+            }
+        }//end of customerMenu
+
+    } //class
+
+
+
+} //namespace
