b49adb015f5067336900899cb65bf2ee02f62a76
jhoxton <jakehnovak@gmail.com>
2018-04-01 13:50:59 +1000

    Partial update done. Need to find correct SQL syntax to process db update next.

--- Franchise.cs
+++ Franchise.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 
 namespace Assignment1
 {
@@ -19,7 +20,7 @@ namespace Assignment1
                 int choice = 0;
                 while (true)
                 {
-                    Console.WriteLine("Welcome to Marvelous Magic(Franchise Holder - " + currentStore.getName() +  ")\n ============== ");
+                    Console.WriteLine("Welcome to Marvelous Magic(Franchise Holder - " + currentStore.getName() + ")\n ============== ");
                     Console.WriteLine("1. Display Inventory");
                     Console.WriteLine("2. Stock Request(Threshold)");
                     Console.WriteLine("3. Add New Inventory Item");
@@ -51,7 +52,7 @@ namespace Assignment1
                             //List<int> storeItemsIntID = new List<int>();
 
                             //currentStore.getStoreInv(storeItemsIntID);
-                            setThreshold(threshold, currentStore );
+                            setThreshold(threshold, currentStore);
 
                             //StockRequest request = new StockRequest(0,0,0,0,true);
 
@@ -78,7 +79,7 @@ namespace Assignment1
                 Console.WriteLine("System Exception : " + e.Message);
             }
         }//end of franchiseMenu
-        public static void franchiseProductPrint( Store currentStore)
+        public static void franchiseProductPrint(Store currentStore)
         {
 
             List<int> storeItemsIntID = new List<int>();//List to locally store items selected stores inventory, via ItemID
@@ -87,7 +88,7 @@ namespace Assignment1
             Console.WriteLine("Inventory");
 
             currentStore.getStoreInv(storeItemsIntID); //Accesses the StoreInventory db to get items and qunatity in store         
-           
+
             // Then creates new items (id, name and quantity) and adds them to the local store objects inventory
 
             Console.WriteLine("{0,-5}  {1,-22} {2,-30}", "ID", "Product", "Current Stock");
@@ -105,7 +106,7 @@ namespace Assignment1
             //franchiseProductPrint(currentStore);
             storeItemsIntID = null;
             //TODO how to stop the store items list doubling up?
- 
+
 
             //Console.WriteLine("Prod input AT END OF PRODUCTPRINT is " + prodInput);
 
@@ -132,7 +133,8 @@ namespace Assignment1
             bool stockFull = false;
 
             string userinput = Console.ReadLine();
-            if (int.TryParse(userinput, out choice)) {
+            if (int.TryParse(userinput, out choice))
+            {
                 restockThreshold = choice;
             }
 
@@ -141,61 +143,133 @@ namespace Assignment1
             foreach (Item i in currentStore.localStoreInventory)
             {
                 //Console.WriteLine(i.getId());//Getting item ID's. If they are higher than userinput, ignore, if lower add to currentStore.thresholdIDs
-                if(i.getStock() <= restockThreshold) {
-                    //int j = i.getId();
+                if (i.getStock() <= restockThreshold)
+                {
+
                     currentStore.thresholdIDs.Add(i.getId());
                     stockFull = true;
                     //foreach(int test in currentStore.thresholdIDs){
                     //    Console.WriteLine(test);
                     //}
-                } else {
+                }
+                else
+                {
                     stockFull = false;
                 }
-            
+
             }
-            if(stockFull == true) {
+            if (stockFull == true)
+            {
 
 
 
 
                 return threshold;
 
-            }   else {
+            }
+            else
+            {
                 Console.WriteLine("All inventory stock levels are equal to or above {0}", restockThreshold);
                 franchiseMenu();
-            }    
+            }
 
-         
-                 
- 
+
+
+
 
 
             return threshold;
 
         }
-        public static void makeStockReguest(Store currentStore, int threshold) {
+        public static void makeStockReguest(Store currentStore, int threshold)
+        {
             Console.WriteLine("{0,-5}  {1,-22} {2,-30}", "ID", "Product", "Current Stock");
-            foreach(Item itemPrint in currentStore.localStoreInventory) {
-                foreach(int idPrint in currentStore.thresholdIDs) {
-                    if(idPrint == itemPrint.getId()) {
-                      
+            foreach (Item itemPrint in currentStore.localStoreInventory)
+            {
+                foreach (int idPrint in currentStore.thresholdIDs)
+                {
+                    if (idPrint == itemPrint.getId())
+                    {
+
 
 
                         Console.WriteLine("{0,-5}  {1,-22} {2,-30}", itemPrint.getId(), itemPrint.getName(), itemPrint.getStock());
 
-                       
+
 
 
                     }
                 }
-                
+
             }
 
             Console.WriteLine();
             Console.WriteLine("Enter request to process: ");
+            string input = Console.ReadLine();
+            int requestToProcess = 0;
+            int choice;
+
+            if (int.TryParse(input, out choice))
+            {
+                requestToProcess = choice;
+            }
+            Console.WriteLine("Item to process is" + requestToProcess);
+            Console.WriteLine("Quanity is" + threshold);
+            //StockRequest request = new StockRequest(0, 0, 0, 0, true);
 
 
-            //Logic here for db call to make new stock request
+            //////
+
+            using (var sqlOp = new SqlConnection("server=wdt2018.australiaeast.cloudapp.azure.com;uid=s3609685;database=s3609685;pwd=abc123"))
+
+            {
+                var stockRequestID = 1;
+                var item = requestToProcess;
+                var quanity = threshold;
+                var productID = 1;
+                //NEED TO FIX THIS?????????
+                //!!!!!!!!!!!!!!!!!!!!!!!?
+
+                sqlOp.Open();
+
+                //var sqlText = @"UPDATE StoreInventory
+                //SET StockLevel = @quantity
+                //WHERE ProductID = @inventoryID
+                //AND StoreID = @storeID";
+
+                var sqlText = @"INSERT INTO StockRequest
+                VALUES(@stockRequestID, @item, @quanity, @productID)";
+
+                //SET StockLevel = @item
+                //WHERE ProductID = @quanity
+                //AND StoreID = @storeID";
+                //INSERT INTO table_name(column1, column2, column3, ...)
+                //VALUES(value1, value2, value3, ...);
+
+                SqlCommand dbCommand = new SqlCommand(sqlText, sqlOp);
+                dbCommand.Parameters.AddWithValue("stockRequestID", stockRequestID);
+                dbCommand.Parameters.AddWithValue("item", item);
+                dbCommand.Parameters.AddWithValue("quanity", quanity);
+
+                dbCommand.Parameters.AddWithValue("productID", productID);
+
+                dbCommand.Connection = sqlOp;
+
+                dbCommand.ExecuteNonQuery();
+
+                sqlOp.Close();
+
+                Console.WriteLine("\n======================\n");
+                return;
+
+            }
         }
+
+
+
+
+        //Logic here for db call to make new stock request
     }
 }
+
+
